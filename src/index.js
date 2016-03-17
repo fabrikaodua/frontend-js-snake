87acee9785b5e1b47f88106b9b4dd5e777ffa7ad
constgen <const.gen@gmail.com>
2016-03-17 11:45:42 +0200

    source template

---
+++ src/index.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+
+var BaseElement = require('./js/base.element.js');
+var Food = require('./js/food.js');
+var SnakePart = require('./js/snake.part.js');
+var Snake = require('./js/snake.js');
+
+console.log(new BaseElement());
+console.log(new Food());
+console.log(new SnakePart());
+console.log(new Snake());
