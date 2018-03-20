0ccbe3a9a81c5c9fb21ec33ff192db7c08c6aeb3
constgen <const.gen@gmail.com>
2018-03-20 02:05:39 +0200

    Fix linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 
 const { Neutrino } = require('neutrino');
 
-module.exports = Neutrino()
-  .use('.neutrinorc.js')
-  .call('eslintrc');
+module.exports = Neutrino({ root: __dirname })
+	.use('.neutrinorc.js')
+	.call('eslintrc');
